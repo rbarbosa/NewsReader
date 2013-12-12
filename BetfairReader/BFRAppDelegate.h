6db74e6a3754bf330f78f92f5087bd0fbd0b7633
Rui Barbosa <rpbarbosa@gmail.com>
2013-12-12 17:37:13 +0000

    Initial commit from local git

---
+++ BetfairReader/BFRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BFRAppDelegate.h
+//  BetfairReader
+//
+//  Created by Rui Pedro Barbosa on 12/12/13.
+//  Copyright (c) 2013 Rui Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BFRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BetfairReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BetfairReader
+//
+//  Created by Rui Pedro Barbosa on 12/12/13.
+//  Copyright (c) 2013 Rui Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BFRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BFRAppDelegate class]));
+    }
+}
